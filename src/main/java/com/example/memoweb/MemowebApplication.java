6d8363170a8c6cd0a9ed7632407724ee85c7da23
meeksfr <mika.sustar@mail.utoronto.ca>
2024-09-17 20:53:11 -0400

    project init

---
+++ src/main/java/com/example/memoweb/MemowebApplication.java
@@ -0,0 +1,13 @@
+package com.example.memoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemowebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/memoweb/MemowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
